1ea0c2025dfcf919660bb5dc336a8aa0ff015d03
Pratik Bhadale <pratikbhadale633@gmail.com>
2025-04-22 19:32:50 +0530

    My first commit after a1

---
+++ src/assignement/A1/EvenNumber.java
@@ -0,0 +1,40 @@
+package assignement.A1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*
+ * 1. Write a program to print whether a number is even or odd, also take
+	input from the user.
+ */
+
+public class EvenNumber {
+	public static boolean isEven(int number) {
+		return number % 2 == 0;
+	}
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Program Started");
+			System.out.println("For exit press any character (A-Z)(a-z)");
+			while (true) {
+				System.out.print("Enter Number : ");
+				int number = sc.nextInt();
+				if (isEven(number)) {
+					System.out.println("Even : " + number);
+				} else {
+					System.out.println("Odd : " + number);
+				}
+			}
+
+		} catch (InputMismatchException ip) {
+			System.out.println("Exiting the Program");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Program Ended");
+		}
+
+	}
+
+}
